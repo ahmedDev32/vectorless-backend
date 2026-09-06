96d26129a991c9978e4fb86b5daa36a23cc68bf7
ahmedmalik-hash <ahmed.malik@berrinex.com>
2026-09-06 18:28:42 +0500

    fix: use --break-system-packages for pip on Vercel's uv-managed Python

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3 -m pip install --break-system-packages -r requirements.txt
+python3 manage.py collectstatic --noinput
